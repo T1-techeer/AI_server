fabbf87d7b09fa11e5bc3ec555973e71bbea0b4f
kookong2 <hy010309@naver.com>
2024-06-01 04:13:02 +0900

    [REFACTOR]gpt 호출 완료

--- chatgpt/routes.py
+++ chatgpt/routes.py
@@ -1,9 +1,14 @@
-
-from fastapi import APIRouter
-
+from fastapi import APIRouter, HTTPException
+from chatgpt.schemas import RefactorRequest
+from chatgpt.services import chat
 
 router = APIRouter(prefix='/api/v1/chatgpt')
 
-# @router.post("/refactor")
-# async def chat_api():
-#     response =
+@router.post("/refactor")
+async def chat_api(request: RefactorRequest):
+    try:
+        # service 코드
+        refactored_code = chat(request.previousCode, request.type)
+        return {"refactored_code": refactored_code}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
