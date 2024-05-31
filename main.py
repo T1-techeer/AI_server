3c4818e276a93cb9e640c957393916964f27a7c8
kookong2 <hy010309@naver.com>
2024-06-01 01:36:03 +0900

    [FEAT] main 파일 기본 설정

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 from fastapi import FastAPI
+from dotenv import load_dotenv
 
+from chatgpt.routes import router as chat_router
+
+import openai
+import os
+
+load_dotenv()
 app = FastAPI()
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+
+
+app.include_router(chat_router)
