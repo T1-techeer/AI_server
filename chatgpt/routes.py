fb726d547f5e0d19e9420500283677d6bb22d6d4
kookong2 <hy010309@naver.com>
2024-06-01 01:51:40 +0900

    [REFACTOR] route 파일 수정

--- chatgpt/routes.py
+++ chatgpt/routes.py
@@ -2,8 +2,8 @@
 from fastapi import APIRouter
 
 
-router = APIRouter(prefix='api/v1/chatgpt')
+router = APIRouter(prefix='/api/v1/chatgpt')
 
-@router.post("/refactor")
-async def chat_api():
-    response = 
+# @router.post("/refactor")
+# async def chat_api():
+#     response =
